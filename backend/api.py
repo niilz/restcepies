1862a783db5f2d221cd2299764d962a0f1f56309
nilSinger <niilz@outlook.de>
2018-11-24 22:25:20 +0100

    experiment with headers

--- backend/api.py
+++ backend/api.py
@@ -14,6 +14,12 @@ CORS(app)
 
 base_url = "https://www.chefkoch.de/rs/s0/"
 id_name_pattern = re.compile(r'rezepte\/(\d+)\/(.+).html')
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.102 Safari/537.36',
+    'Content-Type': 'text/html'
+}
+#headers = {}
+data = None
 
 @app.route('/')
 def collect_recipes():
@@ -23,12 +29,15 @@ def collect_recipes():
         url += f'+{ingredients[str(i)]}'
     url += '/rezepte.html'
     print("URL", url)
-    page = urllib.request.urlopen(url).read().decode('utf-8')
-    recipes = {}
-    for ID, name in id_name_pattern.findall(page):
-        if ID not in recipes:
-            recipes[ID] = name
-    return jsonify(recipes)
+    req = urllib.request.Request(url, data, headers)
+    page = urllib.request.urlopen(req).read()
+    print(page)
+    return "Hello World"
+    # recipes = {}
+    # for ID, name in id_name_pattern.findall(page):
+    #     if ID not in recipes:
+    #         recipes[ID] = name
+    # return jsonify(recipes)
 
 
 
